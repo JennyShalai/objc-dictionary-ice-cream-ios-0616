89d0dcae0e3773dd7668d40ba84ebec3cd84eddc
JennyShalai <jenny.shalai@gmail.com>
2016-05-17 10:36:56 -0400

    add two methods

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,53 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSDictionary *iceCreamPreferencesByPerson = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                                  @"Tim" : @"Natural Vanilla",
+                                                  @"Sophie" : @"Mexican Chocolate",
+                                                  @"Deniz" : @"Natural Vanilla",
+                                                  @"Tom" : @"Mexican Chocolate",
+                                                  @"Jim" : @"Natural Vanilla",
+                                                  @"Mark" : @"Cookies 'n Cream"
+                                                  };
+    
+    NSArray *peopleWhoLikeCurrentIceCream = [NSMutableArray new];
+    NSArray *allValues = [iceCreamPreferencesByPerson allValues];
+    
+    for (NSString *value in allValues) {
+        if ([value isEqualToString:iceCream]) {
+            peopleWhoLikeCurrentIceCream = [iceCreamPreferencesByPerson allKeysForObject:iceCream];
+        }
+    }
+    return peopleWhoLikeCurrentIceCream;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    NSDictionary *iceCreamPreferencesByPerson = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                                  @"Tim" : @"Natural Vanilla",
+                                                  @"Sophie" : @"Mexican Chocolate",
+                                                  @"Deniz" : @"Natural Vanilla",
+                                                  @"Tom" : @"Mexican Chocolate",
+                                                  @"Jim" : @"Natural Vanilla",
+                                                  @"Mark" : @"Cookies 'n Cream"
+                                                  };
+    
+    NSArray *allValues = [iceCreamPreferencesByPerson allValues];
+    NSMutableDictionary *newDictionary = [[NSMutableDictionary alloc] init];
+    for (NSString *value in allValues) {
+        NSMutableArray *usedValues = [[NSMutableArray alloc] init];
+        if (![usedValues containsObject:value]) {
+            NSArray *peopleWhoLikeCurrentIceCream = [self namesForIceCream:value];
+            NSUInteger numberOfPeopleWhoLikeCurrentIceCream = [peopleWhoLikeCurrentIceCream count];
+            NSNumber *temp = @(numberOfPeopleWhoLikeCurrentIceCream);
+            newDictionary[value] = temp;
+            [usedValues addObject:value];
+        }
+    }
+    return newDictionary;
+}
+
 
 @end
